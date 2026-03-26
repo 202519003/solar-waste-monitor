c983130a920ad234a5a537016ec904ed146a0561
202519003 <202519003@dau.ac.in>
2026-03-26 18:27:28 +0530

    Update dashboard.py

--- src/gui/dashboard.py
+++ src/gui/dashboard.py
@@ -135,11 +135,12 @@ with st.sidebar:
         index=0
     )
 
-    year = st.slider(
-    "Select Year",
-    min_value=2023,
-    max_value=2025,
-    value=2025
+    selected_year = st.slider(
+        'Year',
+        min_value=2022,
+        max_value=2025,
+        value=2025,
+        step=1
     )
 
     fire_days = st.selectbox(
